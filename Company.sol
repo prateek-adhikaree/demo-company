935d2decfaf884b690b64fdcebe6062078ea2b60
blocktest-official <contact@blocktest.net>
2018-11-08 10:41:27 -0500

    initial commit

---
+++ Company.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.25;
+
+contract Company {
+
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+        isEmployee[msg.sender] = true;
+    }
+
+    mapping(address => bool) public isEmployee;
+    mapping(address => uint) private employeeToHoursWorked;
+
+    function addEmployee (address _employee) public {
+        require(msg.sender == owner,"adding Denied");
+        isEmployee[_employee] = true;
+    }
+
+    function getHoursLogged (address _employee) public view returns (uint) {
+        require(msg.sender == owner || msg.sender == _employee, "Log Denied");
+        return employeeToHoursWorked[_employee];
+    }
+
+    function logHour(uint _hour) public {
+        require(isEmployee[msg.sender],"Log Failed, Not an Employee");
+        employeeToHoursWorked[msg.sender] += _hour;
+    }
+    
+}
