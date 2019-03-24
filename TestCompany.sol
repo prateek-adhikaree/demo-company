2649715ed9afa2b4fd60c84720e0a5f616c37bca
prateek <prateek.adhikaree@blocktest.net>
2019-03-24 19:38:06 -0400

    adding check summed address

--- TestCompany.sol
+++ TestCompany.sol
@@ -6,7 +6,7 @@ contract TestCompany is Company {
 
     // testing testAddEmployee as constructor as it is a dependency for the other tests
     constructor() {
-        address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+        address employee = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
         if (isEmployee[employee]) {
             isEmployee[employee] = false;
         }
@@ -15,14 +15,14 @@ contract TestCompany is Company {
     }
     
     function testLogHour() public {
-		address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+		address employee = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
         uint totalHours = getHoursLogged(employee);
         logHour(2);
         assert(getHoursLogged(employee) == totalHours + 2);
     }
     
     function testGetHoursLogged () public {
-		address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+		address employee = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
         uint totalHours = getHoursLogged(employee);
         logHour(2);
         assert(getHoursLogged(employee) == totalHours + 2);
