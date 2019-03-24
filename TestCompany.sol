2245786a61a11e411833021a2f6a103089effb05
prateek <prateek.adhikaree@blocktest.net>
2019-03-24 19:15:55 -0400

    empty test contracts

--- TestCompany.sol
+++ TestCompany.sol
@@ -5,25 +5,14 @@ import "./Company.sol";
 contract TestCompany is Company {
     
     function testAddEmployee () public {
-		address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
-        if (isEmployee[employee]) {
-            isEmployee[employee] = false;
-        }
-        addEmployee(employee);
-        assert(isEmployee[employee]);
+		
     }
     
     function testLogHour() public {
-		address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
-        uint totalHours = getHoursLogged(employee);
-        logHour(2);
-        assert(getHoursLogged(employee) == totalHours + 2);
+		
     }
     
     function testGetHoursLogged () public {
-		address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
-        uint totalHours = getHoursLogged(employee);
-        logHour(2);
-        assert(getHoursLogged(employee) == totalHours + 2);
+		
     }
 }
