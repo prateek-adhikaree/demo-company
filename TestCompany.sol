5330f80855e5a715859e0057985fcb2a7990d274
prateek <prateek.adhikaree@blocktest.net>
2019-03-24 19:19:45 -0400

    testAddEmployee added

--- TestCompany.sol
+++ TestCompany.sol
@@ -5,14 +5,17 @@ import "./Company.sol";
 contract TestCompany is Company {
     
     function testAddEmployee () public {
-		
+		address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+        if (isEmployee[employee]) {
+            isEmployee[employee] = false;
+        }
+        addEmployee(employee);
+        assert(isEmployee[employee]);
     }
     
     function testLogHour() public {
-		
     }
     
     function testGetHoursLogged () public {
-		
     }
 }
