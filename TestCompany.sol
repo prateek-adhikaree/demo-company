8a55d1bb56ee25c65ad0a4c4972db3a580cd3735
prateek <prateek.adhikaree@blocktest.net>
2019-03-24 19:36:21 -0400

    adding addEmployee in constructor

--- TestCompany.sol
+++ TestCompany.sol
@@ -3,9 +3,10 @@ pragma solidity 0.4.25;
 import "./Company.sol";
 
 contract TestCompany is Company {
-    
-    function testAddEmployee () public {
-		address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+
+    // testing testAddEmployee as constructor as it is a dependency for the other tests
+    constructor() {
+        address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
         if (isEmployee[employee]) {
             isEmployee[employee] = false;
         }
@@ -14,6 +15,10 @@ contract TestCompany is Company {
     }
     
     function testLogHour() public {
+		address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+        uint totalHours = getHoursLogged(employee);
+        logHour(2);
+        assert(getHoursLogged(employee) == totalHours + 2);
     }
     
     function testGetHoursLogged () public {
